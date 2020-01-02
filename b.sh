d98cd06c82ab0e8b6319983e6cadba7b9ab5338a
Đ`HACK <56754416+DH4CK1@users.noreply.github.com>
2020-01-02 21:09:52 +0700

    Add files via upload

---
+++ b.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+banner(){
+echo $cy"     ╔══════════════════════════╗"
+echo $pu"     ║"$cy" Author"$me":"$pu" Mr.D'HACK"$cy"        ║"
+echo $pu"     ║"$cy" Team"$me":"$pu" Black Coder Crush"$cy"  ║" 
+echo $pu"     ║"$cy" Github"$me":"$pu" DH4CKI"$cy"           ║"
+echo $pu"     ╚══════════════════════════╝"
+echo $me"            * "$pu"ShortLink"$me" *"
+echo $pu"      <══════════"$ku"<"$me"X"$ku">"$cy"═══════════>"
+}
+banner   
